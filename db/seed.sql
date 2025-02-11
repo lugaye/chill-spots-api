e69869b0e0c5b7189c78c622dcb3a08e0ee24023
lugaye <160602222+lugaye@users.noreply.github.com>
2025-02-11 14:50:44 +0300

    Create seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO chill_spots (name, location, description) VALUES
+('The Alchemist', 'Westlands, Nairobi', 'A nightlife spot with live music and food trucks.'),
+('Diani Beach', 'Diani, Kwale', 'A white sandy beach perfect for relaxation.'),
+('Karura Forest', 'Nairobi', 'A serene getaway with trails for hiking and biking.');
